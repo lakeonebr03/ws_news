f77d66744e1613ae2a4f065723e32bf6fd19554c
lakeonebr03 <122464683+lakeonebr03@users.noreply.github.com>
2023-03-01 09:40:23 -0300

    Update ws.py

--- ws.py
+++ ws.py
@@ -9,12 +9,13 @@ soup = BeautifulSoup(site.content, 'html.parser')
 article = soup.find('h3', class_="bbc-1whbtaf")
 headline = article.text
 description = article.find_next("p", class_="bbc-1g5xny6").text
-tweet = headline+"\n"+description+"\n#tweeter #news #brasil "
+git = "By GitHub lakeonebr03/ws_news"
+tweet = (f"{headline}\n{description}\n{git}\n#tweeter #news #brasil")
 #print(tweet)
 ###############
 token = os.getenv('TOKEN_01')
 chat_id = os.getenv('TOKEN_02')
-msg = (f'{tweet}\nBy GitHub lakeonebr03/ws_news')
+msg = (tweet)
 url_tel = ("https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chat_id+"&text="+msg)
 respost = requests.get(url_tel)
 ###############
